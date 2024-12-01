9ce8430618b80e693c70948620f2bbaf29c85f7f
hnayoung <hnaa12@daum.net>
2024-12-01 21:15:57 +0900

    ✨ 앱 라우터 디테일 화면 추가

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Likes from "./pages/Likes";
+import Detail from "./pages/Detail";
 
 export default function AppRouter() {
   return (
@@ -8,6 +9,7 @@ export default function AppRouter() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/likes" element={<Likes />} />
+        <Route path="/detail/:id" element={<Detail />} />
       </Routes>
     </>
   );
